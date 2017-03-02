10495cccaa9edc1e85ed3696613226587e444e47
Dixiat <yangqm1019@gmail.com>
2017-03-02 16:52:53 +0800

    统一服务端文件命名格式，修改部分文件代码格式问题

--- app.js
+++ app.js
@@ -14,91 +14,91 @@ const
     bodyParser = require('koa-bodyparser'),
     historyApiFallback = require('koa-connect-history-api-fallback');
 const
-    staticFiles = require('./server/middlewares/static-files'),
-    router = require('./server/middlewares/router');
+    staticFiles = require('./server/middlewares/staticFiles.js'),
+    router = require('./server/middlewares/router.js');
 
 switch (mode) {
-    case 'development': {
-        const
-            webpack = require('webpack'),
-            webpackConfig = require('./webpack.config.js');
+case 'development': {
+    const
+        webpack = require('webpack'),
+        webpackConfig = require('./webpack.config.js');
 
-        const compiler = webpack(webpackConfig);
+    const compiler = webpack(webpackConfig);
 
-        // import middlewares for development
-        const
-            webpackDev = require('koa-webpack-dev-middleware'),
-            webpackHot = require('koa-webpack-hot-middleware');
+    // import middlewares for development
+    const
+        webpackDev = require('koa-webpack-dev-middleware'),
+        webpackHot = require('koa-webpack-hot-middleware');
 
-        // use middlewares
-        app.use(historyApiFallback({
-            verbose: false,
-            rewrites: [{
-                from: /^\/api\/.*$/,
-                to(context) {
-                    return context.parsedUrl.pathname.substring(4);
-                }
-            }]
-        }));
-        app.use(convert(webpackDev(compiler, {
-            // public path should be the same with webpack config
-            publicPath: webpackConfig.output.publicPath,
-            noInfo: true,
-            stats: {
-                colors: true
+    // use middlewares
+    app.use(historyApiFallback({
+        verbose: false,
+        rewrites: [{
+            from: /^\/api\/.*$/,
+            to(context) {
+                return context.parsedUrl.pathname.substring(4);
             }
-        })));
-        app.use(convert(webpackHot(compiler)));
-        app.use(bodyParser());
-        app.use(router());
-        app.use(staticFiles('/', './build'));
+        }]
+    }));
+    app.use(convert(webpackDev(compiler, {
+        // public path should be the same with webpack config
+        publicPath: webpackConfig.output.publicPath,
+        noInfo: true,
+        stats: {
+            colors: true
+        }
+    })));
+    app.use(convert(webpackHot(compiler)));
+    app.use(bodyParser());
+    app.use(router());
+    app.use(staticFiles('/', './build'));
 
-        //start server
-        app.listen(port, () => {
-            console.log(`App(dev) is now running on port ${port}...`);
-        });
+    // start server
+    app.listen(port, () => {
+        console.log(`App(dev) is now running on port ${port}...`);
+    });
 
-        break;
-    }
-    case 'production': {
+    break;
+}
+case 'production': {
 
-        // use middlewares
-        app.use(historyApiFallback({
-            verbose: false,
-            rewrites: [{
-                from: /^\/api\/.*$/,
-                to(context) {
-                    return context.parsedUrl.pathname.substring(4);
-                }
-            }]
-        }));
-        app.use(bodyParser());
-        app.use(router());
-        app.use(staticFiles('/', './build'));
+    // use middlewares
+    app.use(historyApiFallback({
+        verbose: false,
+        rewrites: [{
+            from: /^\/api\/.*$/,
+            to(context) {
+                return context.parsedUrl.pathname.substring(4);
+            }
+        }]
+    }));
+    app.use(bodyParser());
+    app.use(router());
+    app.use(staticFiles('/', './build'));
 
-        //start server
-        app.listen(port, () => {
-            console.log(`App(production) is running at ${port}...`);
-        });
+    // start server
+    app.listen(port, () => {
+        console.log(`App(production) is running at ${port}...`);
+    });
 
-        break;
-    }
-    case 'api': {
+    break;
+}
+case 'api': {
 
-        // import middlewares for api
-        const rewrite = require('koa-rewrite');
+    // import middlewares for api
+    const rewrite = require('koa-rewrite');
 
-        // use middlewares
-        app.use(rewrite(/^\/api\/(\w+)/, '/$1'));
-        app.use(bodyParser());
-        app.use(router());
+    // use middlewares
+    app.use(rewrite(/^\/api\/(\w+)/, '/$1'));
+    app.use(bodyParser());
+    app.use(router());
 
-        //start server
-        app.listen(port, () => {
-            console.log(`App(api) is running at ${port}...`);
-        });
+    // start server
+    app.listen(port, () => {
+        console.log(`App(api) is running at ${port}...`);
+    });
 
-        break;
-    }
-    default: { break; }
+    break;
+}
+default: { break; }
 }
