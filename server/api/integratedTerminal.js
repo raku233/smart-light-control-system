8045488a3fddab13ef41ca27262f442ac0e6f455
Dixiat <yangqm1019@gmail.com>
2017-03-29 20:47:53 +0800

    添加集中控制模块API文件并更新服务器API模块封装，增添多次请求处理逻辑

---
+++ server/api/integratedTerminal.js
@@ -0,0 +1,14 @@
+module.exports = {
+    GSET_TIME_CLASS: {
+        method: 'get',
+        pathName: '/Onoff/Gset_time_class',
+        param: {},
+        requiredParamKeys: ['Dev_id']
+    },
+    GSET_ONOFF_READ: {
+        method: 'get',
+        pathName: '/Onoff/Gset_Onoff_read',
+        param: {},
+        requiredParamKeys: ['Dev_id']
+    }
+};
