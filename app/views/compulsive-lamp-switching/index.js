a9e7afee414afa5d0cb7f18ad7d6ed73171bd4ea
chunglj <414937885@qq.com>
2017-04-08 01:03:23 +0800

    实现单灯简易控制视图

--- app/views/compulsive-lamp-switching/index.js
+++ app/views/compulsive-lamp-switching/index.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { Button, Slider, Card, Row, Col, Checkbox, Table } from 'antd';
 import { actions as commonActions } from '../../components/common/redux';
 import DeviceList from '../../components/common/device-list';
-
+import SingleLampParameterTable from '../../components/common/single-lamp-parameter-table';
 import './index.css';
 
 const { ColumnGroup, Column } = Table;
@@ -22,20 +22,6 @@ class CompulsiveLampSwitching extends Component {
             100: '100'
         };
 
-        const rowSelection = {
-            onChange: (selectedRowKeys, selectedRows) => {
-                console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-            },
-            onSelect: (record, selected, selectedRows) => {
-                console.log(record, selected, selectedRows);
-            },
-            onSelectAll: (selected, selectedRows, changeRows) => {
-                console.log(selected, selectedRows, changeRows);
-            },
-            getCheckboxProps: record => ({
-                disabled: record.name === 'Disabled User',    // Column configuration not to be checked
-            }),
-        };
         return (
             <div style={{ height: '100%', width: '100%', display: 'flex' }}>
                 <DeviceList {...deviceList} {...deviceListActions}></DeviceList>
@@ -69,20 +55,7 @@ class CompulsiveLampSwitching extends Component {
                         <Button type="primary" className="v-cls-button">查看电流信息</Button>
                         <Button type="primary" className="v-cls-button">查看开关状态</Button>
                     </div>
-                    <div className="v-cls-title">单灯参数信息</div>
-                    <Table style={{ padding: '0 8px' }} rowSelection={rowSelection} >
-                        <Column title="灯杆名" dataIndex="" key="" />
-                        <Column title="灯杆号" dataIndex="" key="" />
-                        <Column title="更新时间" dataIndex="" key="" />
-                        <Column title="1灯电压" dataIndex="" key="" />
-                        <Column title="1灯电流" dataIndex="" key="" />
-                        <Column title="1灯亮度" dataIndex="" key="" />
-                        <Column title="有功功率1" dataIndex="" key="" />
-                        <Column title="无功功率1" dataIndex="" key="" />
-                        <Column title="功率因素1" dataIndex="" key="" />
-                        <Column title="映射杆号" dataIndex="" key="" />
-                        <Column title="开关状态" dataIndex="" key="" />
-                    </Table>
+                    <SingleLampParameterTable />
                 </div>
             </div>
         );
--- app/views/single-lamp-control/index.js
+++ app/views/single-lamp-control/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Row, Col, Card, Switch } from 'antd';
+import { Row, Col, Card, Switch, Button, Checkbox } from 'antd';
 import { actions as commonActions } from '../../components/common/redux';
-
+import SingleLampParameterTable from '../../components/common/single-lamp-parameter-table';
 import DeviceList from '../../components/common/device-list';
+
+import './index.css';
 
 class SingleLampControl extends Component {
     render() {
@@ -15,12 +17,31 @@ class SingleLampControl extends Component {
                 <DeviceList {...deviceList} {...deviceListActions} />
                 <div style={{ flex: 1, height: '100%' }}>
                     <Row>
-                        <Col span="12">
+                        <Col span="10" className="v-slc-col">
                             <Card title={<Switch checkedChildren={'开'} unCheckedChildren={'关'} />}>
-
+                                <div className="v-slc-card-container">
+                                    <Button className="v-slc-button">全手动开灯</Button>
+                                </div>
+                                <div className="v-slc-card-container">
+                                    <Button type="primary" className="v-slc-button">全恢复时控</Button>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col span="14">
+                            <Card title={<Checkbox>对各单灯进行广播</Checkbox>}>
+                                <div className="v-slc-card-container">
+                                    <Button className="v-slc-button">查询时段1</Button>
+                                    <Button type="primary" className="v-slc-button">查询时段2</Button>
+                                </div>
+                                <div className="v-slc-card-container">
+                                    <Button className="v-slc-button">查询开关状态</Button>
+                                    <Button type="primary" className="v-slc-button">查询电流信息</Button>
+                                    <Button className="v-slc-button">查询电杆状态</Button>
+                                </div>
                             </Card>
                         </Col>
                     </Row>
+                    <SingleLampParameterTable />
                 </div>
             </div>
         );
