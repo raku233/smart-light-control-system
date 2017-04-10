a53c425402c0452b2813433064cc9ecf3ea99e38
chunglj <414937885@qq.com>
2017-04-11 01:03:21 +0800

    将单灯控制和单灯地图的组件分离

--- app/views/single-lamp-control/index.js
+++ app/views/single-lamp-control/index.js
@@ -5,7 +5,8 @@ import { Row, Col, Card, Switch, Button, Checkbox } from 'antd';
 import { actions as commonActions } from '../../components/common/redux';
 import SingleLampParameterTable from '../../components/common/single-lamp-parameter-table';
 import DeviceList from '../../components/common/device-list';
-
+import SwitchingCard from '../../components/single-lamp-control/switching-card';
+import QueryCard from '../../components/single-lamp-control/query-card';
 import './index.css';
 
 class SingleLampControl extends Component {
@@ -18,27 +19,10 @@ class SingleLampControl extends Component {
                 <div style={{ flex: 1, height: '100%' }}>
                     <Row>
                         <Col span="10" className="v-slc-col">
-                            <Card title={<Switch checkedChildren={'开'} unCheckedChildren={'关'} />}>
-                                <div className="v-slc-card-container">
-                                    <Button className="v-slc-button">全手动开灯</Button>
-                                </div>
-                                <div className="v-slc-card-container">
-                                    <Button type="primary" className="v-slc-button">全恢复时控</Button>
-                                </div>
-                            </Card>
+                            <SwitchingCard />
                         </Col>
                         <Col span="14">
-                            <Card title={<Checkbox>对各单灯进行广播</Checkbox>}>
-                                <div className="v-slc-card-container">
-                                    <Button className="v-slc-button">查询时段1</Button>
-                                    <Button type="primary" className="v-slc-button">查询时段2</Button>
-                                </div>
-                                <div className="v-slc-card-container">
-                                    <Button className="v-slc-button">查询开关状态</Button>
-                                    <Button type="primary" className="v-slc-button">查询电流信息</Button>
-                                    <Button className="v-slc-button">查询电杆状态</Button>
-                                </div>
-                            </Card>
+                            <QueryCard />
                         </Col>
                     </Row>
                     <SingleLampParameterTable />
--- app/views/single-lamp-map/index.js
+++ app/views/single-lamp-map/index.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Layout, Button } from 'antd';
 import { Map } from 'react-amap';
 import { actions as commonActions } from '../../components/common/redux';
 import DeviceList from '../../components/common/device-list';
-
-const AMAP_KEY = 'a8501da295b13fe4f4d1ebf51da6bf3c';
+import LampMap from '../../components/common/lamp-map';
 
 class SingleLampMap extends Component {
     render() {
         const { deviceList, deviceListActions } = this.props;
-        const plugins = ['Scale', 'OverView', 'ToolBar'];
 
         return (
             <div style={{ height: '100%', width: '100%', display: 'flex' }}>
                 <DeviceList {...deviceList} {...deviceListActions}></DeviceList>
                 <div style={{ flex: 1, height: '100%' }}>
-                    <Map amapkey={AMAP_KEY} plugins={plugins} />
+                    <LampMap />
                 </div>
             </div>
         );
