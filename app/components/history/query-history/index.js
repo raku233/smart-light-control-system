9e865ed7f10e0740263dbf7ddfa5bf63d5dec9a9
chunglj <414937885@qq.com>
2017-04-12 18:48:32 +0800

    去除单灯校时

--- app/components/history/query-history/index.js
+++ app/components/history/query-history/index.js
@@ -10,8 +10,8 @@ class QueryHistory extends Component {
             <div className="c-h-qh-container">
                 <span>起止时间：</span>
                 <RangePicker />
-                <Button className="c-h-qh-button" type="primary">电参数历史查询</Button>
-                <Button className="c-h-qh-button" type="primary">报警历史记录</Button>
+                <Button className="c-h-qh-button" type="primary">电参历史查询</Button>
+                <Button className="c-h-qh-button" type="primary">报警历史查询</Button>
             </div>
         );
     }
