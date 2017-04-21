18bcf697fbb8d972d2354a045c0fd44daf74ca56
Dixiat <yangqm1019@gmail.com>
2017-04-21 10:49:42 +0800

    添加服务端API测试工具

---
+++ server/utils/apiTester.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const polyfill = require('es6-promise').polyfill();
+const fetch = require('isomorphic-fetch');
+
+const parseParam = param => {
+    if (typeof param === 'object') {
+        return JSON.stringify(param);
+    }
+};
+
+const rootURL = 'http://localhost:3000';
+const pathName = '/device_list';
+const param = { DevGroup: '区域分组' };
+const url = rootURL + pathName;
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: parseParam(param)
+}).then(res => {
+    return res.json();
+}).then(value => {
+    console.log('response', value);
+});
