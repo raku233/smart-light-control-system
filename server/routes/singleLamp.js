bac126e64d26fcb8eb5f37c5ed237cae96d4464c
Lxr233 <nnlxr11@163.com>
2017-04-24 20:52:16 +0800

    添加了单灯电参历史的api

--- server/routes/singleLamp.js
+++ server/routes/singleLamp.js
@@ -49,6 +49,8 @@ const fn_fetchSingleAlarmSingleMessage = sharedRouteHandlerGenerator([SINGLELAMP
 /*网页：单灯报警信息——单灯信息 
 const fn_fetchSingleAlarmAllSingleMessage = sharedSpecialRouteHandlerGenerator([SINGLELAMP_API.GET_ALL_SINGLE_ALARM_MES]);*/
 
+
+
 //移动端：单灯故障查询
 const fn_fetchSingleFaultMessage = sharedRouteHandlerGenerator([SINGLELAMP_API. GET_SINGLE_FAULT_MES], undefined, ([data]) => {
     const{single_volt_detail_alarm_his} = data;
@@ -79,6 +81,70 @@ const fn_fetchSingleFaultMessage = sharedRouteHandlerGenerator([SINGLELAMP_API. 
           AlarmList.push(item);
     }
     const returnData = {statusGroup: AlarmList};
+    return JSON.stringify(returnData); 
+});
+
+// 移动端：单灯电参历史 （初始化）
+const fn_fetchSingleParamHistoryInitMessage = sharedRouteHandlerGenerator([SINGLELAMP_API.GET_SINGLE_PARAM_HISTRORY_INIT], undefined, ([data]) => {
+    const { single_volt_detail } = data ;
+    let ParamList = [];
+    let item = {};
+    for(let i = 0; i < single_volt_detail.length; i++) { 
+        item = {}; 
+        for ( let k in single_volt_detail[i] ) {
+            let {update_dtm , rod_num , rod_real , V_rod , V_rod2 , I1, I2, w1, w2} =  single_volt_detail[i] ;
+            //处理返回的时间
+            update_dtm =update_dtm.replace(/[^0-9]/g, '');
+            let date = new Date(parseInt(update_dtm));
+            date = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+(date.getDate())+' '+date.getHours()+':'+date.getMinutes();
+             //devNo:终端号,date时间，rodNum杆号, rodReal末端编码，rodName杆名,alarmInfo警报
+             item = {
+                 date: date,
+                 rodNum: rod_num.toString().trim(),
+                 rodReal: rod_real.toString().trim(),
+                 VRod: V_rod.toString().trim(),
+                 VRod2: V_rod2.toString().trim(),
+                 I1: I1.toString().trim(),
+                 I2: I2.toString().trim(),
+                 w1: w1.toString().trim(),
+                 w2: w2.toString().trim()
+             }
+        }
+        ParamList.push(item);
+    }
+    const returnData = {statusGroup: ParamList};
+    return JSON.stringify(returnData);
+});
+
+// 移动端：单灯电参历史 （查询）
+const fn_fetchSingleParamHistoryQueryMessage = sharedRouteHandlerGenerator([SINGLELAMP_API.GET_SINGLE_PARAM_HISTRORY_QUERY], undefined, ([data]) => {
+    const { single_volt_detail_alarm_his } = data ;
+    let ParamList = [];
+    let item = {};
+    for(let i = 0; i < single_volt_detail_alarm_his.length; i++) { 
+        item = {}; 
+        for ( let k in single_volt_detail_alarm_his[i] ) {
+            let {update_dtm , rod_num , rod_real , V_rod , V_rod2 , I1, I2, w1, w2} =  single_volt_detail_alarm_his[i] ;
+            //处理返回的时间
+            update_dtm =update_dtm.replace(/[^0-9]/g, '');
+            let date = new Date(parseInt(update_dtm));
+            date = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+(date.getDate())+' '+date.getHours()+':'+date.getMinutes();
+             //devNo:终端号,date时间，rodNum杆号, rodReal末端编码，rodName杆名,alarmInfo警报
+             item = {
+                 date: date,
+                 rodNum: rod_num.toString().trim(),
+                 rodReal: rod_real.toString().trim(),
+                 VRod: V_rod.toString().trim(),
+                 VRod2: V_rod2.toString().trim(),
+                 I1: I1.toString().trim(),
+                 I2: I2.toString().trim(),
+                 w1: w1.toString().trim(),
+                 w2: w2.toString().trim()
+             }
+        }
+        ParamList.push(item);
+    }
+    const returnData = {statusGroup: ParamList};
     return JSON.stringify(returnData);
 });
 
@@ -86,5 +152,7 @@ module.exports = {
     'POST /single_alarm_terminal_message/get_status': fn_fetchSingleAlarmTerminalMessage,
     'POST /single_lamp_warning_info/get_status': fn_fetchSingleAlarmSingleMessage,
     'POST /single_fault_query/get_status': fn_fetchSingleFaultMessage,
+    'POST /single_param_history_init/get_status': fn_fetchSingleParamHistoryInitMessage,
+    'POST /single_param_history_query/get_status': fn_fetchSingleParamHistoryQueryMessage,
     //'POST /web_single_lamp_warning_info/get_status' : fn_fetchSingleAlarmAllSingleMessage,
 };
--- server/utils/apiTester.js
+++ server/utils/apiTester.js
@@ -10,9 +10,9 @@ const parseParam = param => {
 };
 
 const rootURL = 'http://localhost:3000';
-const pathName = '/electric_parameter_get';
-const param = { Dev_id: '399' };
-const url = rootURL + pathName;
+const pathName = '/single_param_history_query/get_status';
+const param = { Dev_id:399,begin_date_str:"2017-4-22",end_date_str:"2017-4-23",CurrentPageIndex:0,pageSize:0,rod_num:"1-1",chk_I_time:true,BeginH:5,endH:7,chk_I_zero:false,chk_I_not_zero:false,chk_U_zero:false,chk_U_not_zero:false}; 
+const url = rootURL + pathName ;
 
 fetch(url, {
     method: 'POST',
